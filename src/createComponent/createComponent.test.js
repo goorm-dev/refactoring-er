883fd3c2452209541f82386cbfe08884d1ff884f
gihyeon.lee <gihyeon.lee@goorm.io>
2020-05-23 06:30:56 +0000

    test(createComponent): 유닛 테스트 코드 추가(#2zm7em)

--- src/createComponent/createComponent.test.js
+++ src/createComponent/createComponent.test.js
@@ -1,5 +1,31 @@
+const fs = require('fs-extra');
+
+const Mocker = require('_test/utils/Mocker');
+
+const createComponent = require('./createComponent');
+
 describe('createComponent', () => {
-	test('default', () => {
-		expect(true).toEqual(true);
+	const mkdirp = new Mocker();
+	const writeFile = new Mocker();
+
+	beforeEach(() => {
+		mkdirp
+			.makeSpy({
+				module: fs,
+				funcName: 'mkdirp',
+			})
+			.mockResolvedValue();
+		writeFile
+			.makeSpy({
+				module: fs,
+				funcName: 'writeFile',
+			})
+			.mockResolvedValue();
+	});
+
+	test('COMPONENT_ENTITIES에 따라서 fs.writeFile 호출하는 지', async () => {
+		await createComponent('a');
+
+		expect(writeFile.mock).toHaveBeenCalled();
 	});
 });
